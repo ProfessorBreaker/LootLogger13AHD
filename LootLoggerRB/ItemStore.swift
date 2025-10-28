908e03f68450536b8e6a8c62b33abc4ad1b42675
Professor Breaker <jbreak1@ju.edu>
2025-10-28 10:09:23 -0400

    Persisting the Items

--- LootLoggerRB/ItemStore.swift
+++ LootLoggerRB/ItemStore.swift
@@ -47,15 +47,38 @@ class ItemStore {
         allItems.insert(movedItem, at: toIndex)
     }
     
-    func saveChanges() -> Bool {
+    @objc func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL)")
+        
         do {
                 let encoder = PropertyListEncoder()
                 let data = try encoder.encode(allItems)
+                try data.write(to: itemArchiveURL, options: [.atomic])
+                    print("Saved all of the items")
+                    return true
             } catch let encodingError {
                 print("Error encoding allItems: \(encodingError)")
+                return false
             }
 
-        return false
+        
+    }
+    
+    init() {
+        do {
+                let data = try Data(contentsOf: itemArchiveURL)
+                let unarchiver = PropertyListDecoder()
+                let items = try unarchiver.decode([Item].self, from: data)
+                allItems = items
+            } catch {
+                print("Error reading in saved items: \(error)")
+            }
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self,
+                                       selector: #selector(saveChanges),
+                                       name: UIScene.didEnterBackgroundNotification,
+                                       object: nil)
     }
 //    init() {
 //        for _ in 0..<5 {
